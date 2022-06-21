afb1fa4c51ffa218a8af195795ae90778d775bb6
Wesley Scholl <clients@konjoweb.com>
2022-06-20 22:05:18 -0400

    created nft create functionality

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftmarketaddress = "0xA520441413d77069f82fa4A17EDcDD2b4DFcB6e9"
+export const nftaddress = "0xdC2799058295ba84b5254fEa8D3d1f70586Ebb74"
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Link from 'next/link'
+
+
+function Marketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFT Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-blue-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-500">
+              Sell NFTs
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-blue-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-blue-500">
+              Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default Marketplace
+
+
